aef4b025b9bc4e2c8a72ef4ee59ab5191d4e9c39
Moshe Plotkin <mplotkin@jewpaltz.com>
2017-09-28 12:31:15 -0400

    sass, npm scripts, express

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
-const http = require("http");
-const _ = require("lodash");
+const http = require("express");
 const handler = require("./httpHandler.js")
 
-const server = http.createServer(handler.main);
+const server = http();
+
+server.use("/json", function(req, res, next){
+    res.send({ happy: "Yom Kippur"})
+    next();
+});
+
+server.use("/old", handler.main);
+    
 
 server.listen(3000);
+
+console.log("http://localhost:3000");
