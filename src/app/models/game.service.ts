9c36bea83b2a4164e178ba28750370e915d2cb6a
Rabbi Moshe Plotkin <mplotkin@jewpaltz.com>
2017-11-29 22:30:33 +0000

    changes for cloud 9

--- src/app/models/game.service.ts
+++ src/app/models/game.service.ts
@@ -9,10 +9,11 @@ declare var FB: any;
 @Injectable()
 export class GameService {
 
-    apiRoot = "//localhost:3001"    
+    apiRoot: string;   
     me: Player;
 
     constructor(private http: Http, private router: Router) {
+        this.apiRoot = `//${window.location.hostname}:8081` 
         window.fbAsyncInit = function() {
             FB.init({
               appId      : '954556401368395',
